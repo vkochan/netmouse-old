4840a217874dc1943f988740242608a133291d13
vkochan <vadim4j@gmail.com>
2012-07-17 23:47:18 +0300

    [code cleanup] removed service.c & moved code from there to netmouse.c as main app module

--- src/netmouse.c
+++ src/netmouse.c
@@ -21,10 +21,69 @@
 #include <windows.h>
 #include <stdio.h>
 
-#include "service.h"
+#include "event_pipe.h"
+#include "event_receiver.h"
+#include "event_sender.h"
+#include "process.h"
+#include "config.h"
+#include "log.h"
+
+DECLARE_PROCESS(main_app_loop);
+
+/* 
+ * processes windows message queue
+ * in separated thread
+ */ 
+void message_loop()
+{
+	MSG message;
+	while (GetMessage(&message,NULL,0,0)) {
+		TranslateMessage( &message );
+		DispatchMessage( &message );
+	}
+}
+
+/* 
+ * windows messages loop procedure
+ */
+DWORD WINAPI main_app_loop(LPVOID lpParm)
+{	
+	init_event_pipe();
+	
+	init_event_receiver();
+	
+	init_event_sender();
+	
+   //prevents event sending if its server ... 
+   //may be its bad & it should be fixed
+	if (get_server_addr())
+	{
+		init_input_handler();
+	}
+	
+	message_loop();
+	
+	cleanup_input_handler();
+	
+	return 0;
+}
+ 
+void run_main_app(int argc, char **cmdl)
+{	
+	LOG_INFO("NetMouse by Vadim Kochan <vadim4j@gmail.com>\n\n");
+	
+	load_config(argc, cmdl);
+	
+	LOG_INFO("server=%s\n", get_server_addr());
+	LOG_INFO("port=%s\n\n", get_server_port());
+	
+	RUN_PROCESS(main_app_loop, NULL);
+	
+	getchar();
+}
 
 int main(int argc, char **cmdLine)
 {
-	start_app_service(argc, cmdLine);
+	run_main_app(argc, cmdLine);
 	return 0;
 }
