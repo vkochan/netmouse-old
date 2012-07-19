cbc7544ea8a0741a15b5b6ea1e35fe8ec52430b6
vkochan <vadim4j@gmail.com>
2012-07-19 17:23:22 +0300

    updated build.sh - copy target app exe into root folder - cleanup compiled objects from src folder
    source $VIMRUNTIME/vimrc_example.vim

--- build.sh
+++ build.sh
@@ -3,7 +3,12 @@
 MAKE=mingw32-make
 MFLAGS=-C
 SRCDIR=src
+PROGRAM=netmouse.exe
 
+rm $PROGRAM
+rm $SRCDIR/$PROGRAM
+
+$MAKE $MFLAGS $SRCDIR
+
+cp $SRCDIR/$PROGRAM .
 rm $SRCDIR/*.o
-
-$MAKE $MFLAGS $SRCDIR 
